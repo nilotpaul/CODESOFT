5d16f566472151444562a1e6a0fc3d8b6ee6bfa6
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2023-10-06 20:14:51 +0530

    bug fixes

--- Travel Booking/frontend/vite.config.js
+++ Travel Booking/frontend/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://coral-app-ibtww.ondigitalocean.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
